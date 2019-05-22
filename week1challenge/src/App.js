0ca4362135b07e7be06d0f4c5d6af8c2bc207e6e
Theodore Rose <ter9hb@virginia.edu>
2019-05-22 10:26:36 -0400

    first-commit

--- week1challenge/src/App.js
+++ week1challenge/src/App.js
@@ -1,26 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import ReactLeafletMap from "./ReactLeafletMap.js";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurants: [],
+      category: "",
+      coords: []
+    };
+  }
+
+  changeParent = (field, value) => {
+    this.setState({
+      [field]: value
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    });
+  };
+
+  componentDidMount() {
+    axios
+      .get(
+        "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.029649,-78.476841&radius=5000&opennow&type=restaurant&key=+" +
+          API_KEY
+      )
+      .then(response => {
+        let coords = [];
+        try {
+          for (let i = 0; i < response.data.results.length; i++) {
+            coords.push({
+              coordinates: [
+                response.data.results[i].geometry.location.lat,
+                response.data.results[i].geometry.location.lng
+              ],
+              names: response.data.results[i].name,
+              open_now: response.data.results[i].opening_hours.open_now
+            });
+          }
+        } catch (err) {
+          console.log(err);
+        }
+
+        this.setState({
+          restaurants: response.data.results,
+          coords: coords
+        });
+      });
+  }
+
+  formatRestaurants = () => {
+    let restaurant_list = this.state.restaurants;
+
+    return restaurant_list.map(location => {
+      try {
+        if (location.opening_hours.open_now != null)
+          return (
+            <li>
+              <b> Name: </b> {location.name} <b>Rating: </b>
+              {location.rating} <b>Price Level: </b>
+              {location.price_level}
+            </li>
+          );
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  };
+
+  handleChange = event => {
+    this.setState({
+      category: event.target.value
+    });
+  };
+
+  handleClick = event => {
+    this.componentDidMount();
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          Category
+          <label>
+            <input
+              type="text"
+              value={this.state.category}
+              onChange={this.handleChange}
+            />
+          </label>
+        </form>
+        <button onClick={this.handleClick}>Submit</button>
+
+        {this.formatRestaurants()}
+
+        <ReactLeafletMap coords={this.state.coords} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ week1challenge/src/ReactLeafletMap.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+import { Map, Marker, CircleMarker, TileLayer, Tooltip } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default class ReactLeafletMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    console.log(this.props.coords);
+    return (
+      <div>
+        <Map
+          style={{ height: "480px", width: "100%" }}
+          zoom={14}
+          center={[38.03, -78.48]}
+        >
+          <TileLayer url="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+          {this.props.coords.map(location => {
+            return (
+              <div>
+                <CircleMarker
+                  center={[
+                    location["coordinates"][0],
+                    location["coordinates"][1]
+                  ]}
+                  radius={5}
+                >
+                  <Tooltip direction="right" offset={[-8, -2]} opacity={11}>
+                    <span>{location["names"]}</span>
+                  </Tooltip>
+                </CircleMarker>
+              </div>
+            );
+          })}
+        </Map>
+      </div>
+    );
+  }
+}
